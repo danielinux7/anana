ff7ae02561ff074a3477ce83b26cea5ec72a8497
Nart Tlisha <daniel.abzakh@gmail.com>
2021-10-22 14:30:14 +0300

    Added draft folder

---
+++ draft/process-text.sh
@@ -0,0 +1,49 @@
+for file in *.txt;
+do
+  ### Cleaning
+  sed -i -e '/^$/d' $file;
+  sed -i -e '/^[0-9]*$/d' $file;
+  sed -i -r 's/^[0-9]*([[:alpha:][:punct:]])/\1/g' $file;
+  ### Splitting
+  sed -i -z 's/\xC2\xAD\n//g' $file;
+  sed -i -r 's/[-_—]\n//g' $file;
+  sed -i -z 's/\n/ /g' $file;
+  sed -i -r 's/([:!?]) /\1\n/g' $file;
+  sed -i -e 's/\.\.\. /...\n/g' -e 's/\.\. /..\n/g' -e 's/\.\.\.» /...»\n/g' -e 's/\.\.» /..»\n/g' $file;
+  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]\.) /\1\n/g' $file;
+  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]\.\)) /\1\n/g' $file;
+  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]»\.\)) /\1\n/g' $file;
+  sed -i -r 's/([?!»\)]\.) /\1\n/g' $file;
+  sed -i -r 's/ –/\n–/g' $file;
+  ### Cleaning
+  sed -i -e 's/;/,/g' -e 's/:/./g' -e 's/“/«/g' -e 's/”/»/g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' \
+  -e 's/\xC2\xAD//g' -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' -e 's/\*//g' -e 's/^– //g' -e 's/,$/./g' \
+  -e 's/[«»\(\)]//g' -e 's/— //g' -e 's/…/.../g' -e 's/№//g' -e 's/_//g' -e 's/—//g' $file;
+  sed -i -r 's/([[:alpha:]])$/\1./g' $file;
+  sed -i -r 's/[ ]+/ /g' $file;
+  sed -i -r 's/^[ ]+//g' $file;
+  sed -i -r 's/^([[:alpha:]])/\U\1/g' $file;
+  sed -i -e 's/^\.\.\.//g' $file
+  sed -i -r 's/[\.]+$/./g' $file
+  sed -i -e '/^[[:punct:] ]*$/d' $file;
+  ### Sorting and removing duplicates
+  sort $file | uniq > $file+2;
+  cat $file+2 | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- > $file;
+  rm $file+2;
+  ## here is more work needed.
+  ##split -l 500 -d $file ${file/.txt//}
+  ### Show all printed symbols in the files
+  sed -e 's/\(.*\)/\L\1/' $file | grep -o '[[:print:]]' | sort | uniq
+done
+
+# cat *.txt > all
+# mv all all.txt
+# for file in all.txt;
+# do
+#   ### Sorting and removing duplicates
+#   sort $file | uniq > $file+2;
+#   cat $file+2 | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- > $file;
+#   rm $file+2;
+#   ### Show all printed symbols in the files
+#   sed -e 's/\(.*\)/\L\1/' $file | grep -o '[[:print:]]' | sort | uniq
+# done
