a85e54024132c4e93dbf398f77c1082235269db1
Nart Tlisha <daniel.abzakh@gmail.com>
2022-01-27 15:08:46 +0300

    Enhancement 6

--- draft/process-text.sh
+++ draft/process-text.sh
@@ -12,7 +12,7 @@ do
   sed -i -r 's/\xE2\x80\x89/ /g' $file;
   sed -i -z 's/\xCC\x81//g' $file;
   sed -i -z 's/\x0C//g' $file;
-  sed -i -z 's/\xC2\xAD\n//g' $file;
+  sed -i -z 's/\xC2\xAD[\n]*//g' $file;
   sed -i -e 's/−/–/g' $file;
   sed -i -r 's/^[0-9]*([[:alpha:][:punct:]])/\1/g' $file;
   sed -i -r 's/–([[:graph:]])/– \1/g' $file;
@@ -29,7 +29,7 @@ do
   sed -i -r 's/ –/\n–/g' $file;
   ### Cleaning
   sed -i -e 's/;/,/g' -e 's/:/./g' -e 's/“/«/g' -e 's/”/»/g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' \
-  -e 's/\xC2\xAD//g' -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' -e 's/\*//g' -e 's/^– //g' \
+  -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' -e 's/\*//g' -e 's/^– //g' \
   -e 's/[«»\(\)]//g' -e 's/— //g' -e 's/…/.../g' -e 's/№//g' -e 's/_//g' -e 's/—//g' $file;
   sed -i -r 's/([[:alpha:]])$/\1./g' $file;
   sed -i -r 's/[ ]+/ /g' $file;
