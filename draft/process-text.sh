a260f00d573414fc92d5ee873d81c64cb33e560d
Nart Tlisha <daniel.abzakh@gmail.com>
2022-01-27 16:42:41 +0300

    Enhancement 10

--- draft/process-text.sh
+++ draft/process-text.sh
@@ -16,19 +16,14 @@ do
   ### Splitting
   sed -i -z 's/[-_—−]\n//g' $file;
   sed -i -z 's/\n/ /g' $file;
-  sed -i -e 's/\.\.\. /...\n/g' -e 's/\.\. /..\n/g' -e 's/\.\.\.» /...»\n/g' -e 's/\.\.» /..»\n/g' $file;
-  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]\.) /\1\n/g' $file;
-  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]\.\)) /\1\n/g' $file;
-  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]»\.\)) /\1\n/g' $file;
   sed -i -r 's/([?!»\)]\.) /\1\n/g' $file;
   sed -i -r 's/ –/\n–/g' $file;
   ### Cleaning
-  sed -i -e 's/;/,/g' -e 's/:/./g' -e 's/“/«/g' -e 's/”/»/g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' \
+  sed -i -e 's/;/,/g' -e 's/:/./g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' \
   -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' -e 's/\*//g' -e 's/^– //g' \
-  -e 's/[«»\(\)]//g' -e 's/— //g' -e 's/…/.../g' -e 's/№//g' -e 's/_//g' -e 's/—//g' $file;
+  -e 's/[«»“”\(\)]//g' -e 's/— //g' -e 's/…/.../g' -e 's/№//g' -e 's/_//g' -e 's/—//g' $file;
   sed -i -r 's/([[:alpha:]])$/\1./g' $file;
-  sed -i -r 's/(\.[\.]+)([[:alpha:]])/\1 \2/g' $file;
-  sed -i -e 's/\.\.\. /…\n/g' -e 's/\.\. /…\n/g' $file;
+  sed -i -e 's/\.\.\./…\n/g' -e 's/\.\./…\n/g' $file;
   sed -i -r 's/([!?])[,\.]+/\1/g' $file;
   sed -i -r 's/ ([,!?\.])/\1/g' $file;
   sed -i -r 's/,([[:alpha:]])/, \1/g' $file;
@@ -40,7 +35,7 @@ do
   sed -i -r 's/([[:alpha:]]{3,}\.)/\1\n/g' $file;
   sed -i -r 's/[ ]+/ /g' $file;
   sed -i -r 's/^[ ]+//g' $file;
-  sed -i -e 's/^\([[:alpha:]]\)/\U\1/' $file
+  sed -i -r 's/^(['$alpha'])/\U\1/' $file
   ### Sorting and removing duplicates
   sort $file | uniq > $file+2;
   cat $file+2 | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- > $file;
