636301d3125905fa9ef51a8f5c28ce75cbc3ecfb
nart <daniel.abzakh@gmail.com>
2022-02-02 12:52:34 +0300

    Enhancement 16

--- draft/process-text.sh
+++ draft/process-text.sh
@@ -7,31 +7,31 @@ done
 
 for file in $(ls | grep '^[0-9]\+.txt.temp$')
 do
-  ### Cleaning
+  ### preprocess
   sed -ni '/['$alpha']/p' $file;
   sed -i -r 's/\xE2\x80\x89/ /g' $file;
   sed -i -z 's/\xCC\x81//g' $file;
   sed -i -z 's/\x0C//g' $file;
   sed -i -z 's/\xC2\xAD[\n]*//g' $file;
-  sed -i -e 's/[…:;]/.../g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' $file;
+  sed -i -e 's/[:;]/…/g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' $file;
+  sed -i -r 's/[\.]{2,3}/…/g' $file;
   sed -i -r 's/[,]+/,/g' $file;
   sed -i -r 's/[!]+/!/g' $file;
   sed -i -r 's/[?]+/?/g' $file;
   sed -i -r 's/,([[:alpha:]])/, \1/g' $file;
   sed -i -r 's/([!?])[,\.]+/\1/g' $file;
-  sed -i -r 's/ ([,!?\.])/\1/g' $file;
+  sed -i -r 's/ ([…,!?\.])/\1/g' $file;
   sed -i -e 's/- / /g' -e 's/ - / /g' $file;
   sed -i -e 's/[«»“”\(\)\*№]//g' $file;
   ### Splitting
   sed -i -z 's/[-_—−]\n//g' $file;
   sed -i -z 's/\n/ /g' $file;
-  sed -i -r 's/([?!])[\. ]/\1\n/g' $file;
-  sed -i -e 's/\.\.\./…\n/g' -e 's/\.\./…\n/g' $file;
+  sed -i -r 's/([?!…])[\. ]/\1\n/g' $file;
   sed -i -r 's/([[:alpha:]]{3,}\.)/\1\n/g' $file;
-  sed -i -r 's/[ ]–[ ]/…\n/g' $file;
+  sed -i -r 's/[ ]–[ ]/\n/g' $file;
   sed -i -e 's/[—_–]//g' $file;
-  ### Cleaning
-  sed -i -r 's/([[:alpha:]])$/\1./g' $file;
+  ### postprocess
+  sed -i -r 's/([[:alpha:]])[,]*$/\1…/g' $file;
   sed -i -r 's/[ ]+/ /g' $file;
   sed -i -r 's/^[ ]+//g' $file;
   sed -i -r 's/^(['$alpha'])/\U\1/' $file
