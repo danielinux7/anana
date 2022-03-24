ecacd57fe2774f2b27cf931b291bc8816e6f8aaf
Nart <daniel.abzakh@gmail.com>
2022-03-24 15:10:43 +0300

    Edited script 2

--- nmt/draft/process-text.py
+++ nmt/draft/process-text.py
@@ -7,15 +7,16 @@ tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
 
 for file in os.scandir('.'):
     if file.is_file() & bool(f.match(file.name)):
-        with open(file.name, 'r', newline='') as tsvFile, tempfile:
+        with open(file.name, 'r', newline='') as tsvFile:
             reader = csv.reader(tsvFile, delimiter='\t')
-            writer = csv.writer(tempfile, delimiter='\t')
-            for row in reader:
-                row[0] = p.split(row[0])
-                row[1] = p.split(row[1])
-                max = len(row[0]) if len(row[0]) > len(row[1]) else len(row[1])
-                for i in range(max):
-                    value1 = row[0][i].strip() if i < len(row[0]) else ""
-                    value2 = row[1][i].strip() if i < len(row[1]) else ""
-                    writer.writerow([value1, value2])
-        shutil.move(tempfile.name, file.name+'.temp')
+            with open(tempfile.name, 'w', newline='') as tempfile:
+                writer = csv.writer(tempfile, delimiter='\t')
+                for row in reader:
+                    row[0] = p.split(row[0])
+                    row[1] = p.split(row[1])
+                    max = len(row[0]) if len(row[0]) > len(row[1]) else len(row[1])
+                    for i in range(max):
+                        value1 = row[0][i].strip() if i < len(row[0]) else ""
+                        value2 = row[1][i].strip() if i < len(row[1]) else ""
+                        writer.writerow([value1, value2])
+            shutil.move(tempfile.name, file.name+'.temp')
