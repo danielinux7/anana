039cbf122a5dbe0ac19cd3a9ee22de6b0ceda2ef
Nart <daniel.abzakh@gmail.com>
2022-03-24 13:54:54 +0300

    Edited script

--- nmt/draft/process-text.py
+++ nmt/draft/process-text.py
@@ -1,16 +1,21 @@
 from tempfile import NamedTemporaryFile
-import shutil, csv, re
+import shutil, csv, re, os
 
-filename = 'test.tsv'
+p = re.compile('#')
+f = re.compile('[0-9]*.tsv')
 tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
 
-with open(filename, 'r', newline='') as tsvFile, tempfile:
-    reader = csv.reader(tsvFile, delimiter='\t')
-    writer = csv.writer(tempfile, delimiter='\t')
-
-    for row in reader:
-        row[1] = row[1].title()
-        print(row[0])
-        writer.writerow(row)
-
-shutil.move(tempfile.name, 'test2.tsv')
+for file in os.scandir('.'):
+    if file.is_file() & bool(f.match(file.name)):
+        with open(file.name, 'r', newline='') as tsvFile, tempfile:
+            reader = csv.reader(tsvFile, delimiter='\t')
+            writer = csv.writer(tempfile, delimiter='\t')
+            for row in reader:
+                row[0] = p.split(row[0])
+                row[1] = p.split(row[1])
+                max = len(row[0]) if len(row[0]) > len(row[1]) else len(row[1])
+                for i in range(max):
+                    value1 = row[0][i].strip() if i < len(row[0]) else ""
+                    value2 = row[1][i].strip() if i < len(row[1]) else ""
+                    writer.writerow([value1, value2])
+        shutil.move(tempfile.name, file.name+'.temp')
--- nmt/draft/process-text.sh
+++ nmt/draft/process-text.sh
@@ -1,64 +1,6 @@
-# Manual cleanup
-# [_—−–]$ and [_]
-# [ёйщъэяю]
-# 4: Chane Alpha to ALPHA for misplaced single words in a line.
-rm *.txt
-for file in $(ls | grep '^[0-9]\+.tsv$');
-do
-  sed -r 's/\t.*//' $file > ${file/.tsv/}.ab.txt
-  sed -r 's/.*\t//' $file > ${file/.tsv/}.ru.txt
-done
-
-for file in $(ls | grep '^[0-9]\+.ab.txt$')
+for file in $(ls | grep '^[0-9]\+.tsv$')
 do
   ### preprocess
-  sed -i -r 's/([[:alpha:]])[,]*$/\1…/g' $file;
-  # 4
-  sed -i -r '/^[АӘБВГӶДЕЖЗӠИКҚҞЛМНОПԤРСТҬУФХҲЦҴЧҶҼҾЏШЫЬҨ ]+$/s/.*//' $file;
-  sed -i -r 's/\xE2\x80\x89/ /g' $file;
-  sed -i -z 's/\xCC\x81//g' $file;
-  sed -i -z 's/\x0C//g' $file;
-  sed -i -z 's/\xC2\xAD[\n]*//g' $file;
-  sed -i -e 's/[:;]/…/g' -e 's/ҕ/ӷ/g' -e 's/Ҕ/Ӷ/g' -e 's/ҧ/ԥ/g' -e 's/Ҧ/Ԥ/g' $file;
-  sed -i -r 's/[\.]{2,3}/…/g' $file;
-  sed -i -r 's/[,]+/,/g' $file;
-  sed -i -r 's/[!]+/!/g' $file;
-  sed -i -r 's/[?]+/?/g' $file;
-  sed -i -r 's/,([[:alpha:]])/, \1/g' $file;
-  sed -i -r 's/([!?])[,\.]+/\1/g' $file;
-  sed -i -r 's/ ([…,!?\.])/\1/g' $file;
-  sed -i -e 's/- / /g' -e 's/ - / /g' $file;
-  sed -i -e 's/[«»"“”\(\)\*№]//g' $file;
-  sed -i -r 's/([[:alpha:]])–([[:alpha:]])/\1-\2/g' $file;
-  ### Splitting
-  sed -i -z 's/[-]\n//g' $file;
-  # 1
-  sed -i -z 's/\n/ /g' $file;
-  sed -i -r 's/([?!…][!]*)|–[ ]*([[:upper:]])/\1\n\2/g' $file;
-  sed -i -r 's/([[:alpha:]]{3,}\.)/\1\n/g' $file;
-  ### postprocess
-  # 2
-  sed -i -r 's/–/ /g' $file;
-  sed -i -r 's/,[ ]*\b([аисршлҳ]ҳә[аое][и]*[нтп])\b/, – \1/g' $file;
-  # 3
-  sed -i -r 's/^.*[0-9]+[ ,]*([[:upper:]])/\1/g' $file;
-  #
-  sed -i -r 's/^[ ]+|[ ]+$//g' $file;
-  sed -i -r 's/([[:alpha:]])[,]*$/\1…/g' $file;
-  sed -i -r 's/[ ]+/ /g' $file;
-  sed -i -r 's/^([[:alpha:]])/\U\1/' $file
-done
-
-for file in $(ls | grep '^[0-9]\+.ru.txt$')
-do
-  ### preprocess
-  sed -i -r 's/([[:alpha:]])[,]*$/\1…/g' $file;
-  # 4
-  sed -i -r '/^[ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ ]+$/s/.*//' $file;
-  sed -i -r 's/\xE2\x80\x89/ /g' $file;
-  sed -i -z 's/\xCC\x81//g' $file;
-  sed -i -z 's/\x0C//g' $file;
-  sed -i -z 's/\xC2\xAD[\n]*//g' $file;
   sed -i -e 's/[:;]/…/g' $file;
   sed -i -r 's/[\.]{2,3}/…/g' $file;
   sed -i -r 's/[,]+/,/g' $file;
@@ -70,20 +12,12 @@ do
   sed -i -e 's/- / /g' -e 's/ - / /g' $file;
   sed -i -e 's/[«»"“”\(\)\*№]//g' $file;
   sed -i -r 's/([[:alpha:]])–([[:alpha:]])/\1-\2/g' $file;
+  sed -z -i -r 's/([[:alpha:][:digit:]])[,]*([\t\n])/\1…\2/g' $file;
   ### Splitting
-  sed -i -z 's/[-]\n//g' $file;
-  # 1
-  sed -i -z 's/\n/ /g' $file;
-  sed -i -r 's/([?!…][!]*)|–[ ]*([[:upper:]])/\1\n\2/g' $file;
-  sed -i -r 's/([[:alpha:]]{3,}\.)/\1\n/g' $file;
+  sed -i -r 's/([?!…][!]*)/\1#/g' $file;
+  sed -i -r 's/([[:alpha:]]{3,}\.)/\1#/g' $file;
   ### postprocess
-  # 2
-  sed -i -r 's/–/ /g' $file;
-  # 3
-  sed -i -r 's/^.*[0-9]+[ ,]*([[:upper:]])/\1/g' $file;
-  #
+  sed -z -i -r 's/#([\t\n])/\1/g' $file;
   sed -i -r 's/^[ ]+|[ ]+$//g' $file;
-  sed -i -r 's/([[:alpha:]])[,]*$/\1…/g' $file;
   sed -i -r 's/[ ]+/ /g' $file;
-  sed -i -r 's/^([[:alpha:]])/\U\1/' $file
 done
