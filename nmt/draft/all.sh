b1cac17f9e52b82b1c5ec3237e4f3c69fa67cdb1
Nart <daniel.abzakh@gmail.com>
2022-03-25 19:20:44 +0300

    Edited script 8

--- nmt/draft/all.sh
+++ nmt/draft/all.sh
@@ -7,7 +7,7 @@ done
 for file in $(ls | grep '^[0-9]\+.tsv.clean$');
 do
   # Remove all but the following symbols and patterns
-  sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]$/I!d' $file;
+  sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]\t0$/I!d' $file;
   sed -i -r '/\tҲәа[н, ]|\t[АИСРШЛҲ]ҳә[аое]|[[:alpha:]]-[[:alpha:]]+-[[:alpha:]]|\b[[:alpha:]]\b|[[:upper:]][[:upper:]]/d' $file;
   ### Replace more or less than 10-90 characters.
   sed -i -r '/^.{,20}$|^.{200,}$/d' $file;
