3ef6508838abe9316d542506ce61e36281291dfc
Nart <daniel.abzakh@gmail.com>
2022-03-28 12:29:19 +0300

    Edited script 2

--- nmt/draft/all.sh
+++ nmt/draft/all.sh
@@ -1,20 +1,12 @@
-sed -i -r 's/[^[:print:]\t]//g' [0-9]*.tsv.temp;
-for file in $(ls | grep '^[0-9]\+.tsv.temp$');
-do
-  cp $file ${file/temp/clean};
-done
+sed -r 's/[^[:print:]\t]//g' [0-9]*.tsv.temp|cat - > all.tsv.temp;
+cp  all.tsv.temp  all.tsv.clean.temp
+file="all.tsv.clean.temp"
+# Remove all but the following symbols and patterns
+sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]\t0$/I!d' $file;
+sed -i -r '/\tҲәа[н, ]|\t[АИСРШЛҲ]ҳә[аое]|[[:alpha:]]-[[:alpha:]]+-[[:alpha:]]|\b[[:alpha:]]\b|[[:upper:]][[:upper:]]/d' $file;
+### Replace more or less than 10-90 characters.
+sed -i -r '/^.{,20}$|^.{200,}$/d' $file;
 
-for file in $(ls | grep '^[0-9]\+.tsv.clean$');
-do
-  # Process sentences
-  sed -i -r 's/\t[[:punct:] ]*([[:alpha:]])/\U\1/g' $file;
-  # Remove all but the following symbols and patterns
-  sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]\t0$/I!d' $file;
-  sed -i -r '/\tҲәа[н, ]|\t[АИСРШЛҲ]ҳә[аое]|[[:alpha:]]-[[:alpha:]]+-[[:alpha:]]|\b[[:alpha:]]\b|[[:upper:]][[:upper:]]/d' $file;
-  ### Replace more or less than 10-90 characters.
-  sed -i -r '/^.{,20}$|^.{200,}$/d' $file;
-done
-
-cat [0-9]*.tsv.temp > all.tsv.temp
-cat [0-9]*.tsv.clean | shuf > all.tsv.clean
-rm [0-9]*.tsv.clean [0-9]*.tsv.temp
+cat all.tsv.clean.temp | shuf > all.tsv.clean
+touch 999.tsv.temp
+rm [0-9]*.tsv.temp all.tsv.clean.temp
