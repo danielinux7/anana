fc96c0e4ee32be944fd7e301064498eae2bc9187
Nart <daniel.abzakh@gmail.com>
2022-03-28 14:57:57 +0300

    Edited script 5

--- nmt/draft/all.sh
+++ nmt/draft/all.sh
@@ -6,5 +6,9 @@ file="all.tsv.clean.temp"
 # Remove all but the following symbols and patterns
 sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]\t0$/I!d' $file;
 sed -i -r '/\tҲәа[н, ]|\t[АИСРШЛҲ]ҳә[аое]|[[:alpha:]]-[[:alpha:]]+-[[:alpha:]]|\b[[:alpha:]]\b|[[:upper:]][[:upper:]]/d' $file;
-### Replace more or less than 10-90 characters.
-sed -r '/^.{,20}$|^.{200,}$/d' $file | shuf > all.tsv.clean && rm all.tsv.clean.temp;
+# Remove duplicates from source
+cut -f2 $file | sed -e 's/[[:punct:]]//g' | sed 's/./\L&/g' | \
+paste $file - > $file.temp;
+sort -t$'\t' -k3 -u $file.temp | cut -f1,2,3,4 | shuf > $file && rm $file.temp;
+### Replace more or less than 30-200 characters.
+sed -r '/^.{,30}$|^.{200,}$/d' $file | shuf > all.tsv.clean && rm all.tsv.clean.temp;
