9edb29db02bf1634012edab9fe036c4a5b4d7747
Nart <daniel.abzakh@gmail.com>
2022-03-30 13:33:10 +0300

    Edited script 4

--- nmt/draft/all.sh
+++ nmt/draft/all.sh
@@ -7,8 +7,8 @@ file="all.tsv.clean.temp"
 sed -i -r '/^[0-9+-]*\t[аәбвгӷдежзӡикқҟлмнопԥрстҭуфхҳцҵчҷҽҿџшыьҩ ,?–-]*[…\.!?]\t[ёйцукенгшщзхъфывапролджэячсмитьбю ,?–-]*[…\.!?]\t0$/I!d' $file;
 sed -i -r '/\tҲәа[н, ]|\t[АИСРШЛҲ]ҳә[аое]|[[:alpha:]]-[[:alpha:]]+-[[:alpha:]]|^[0-9]*\t[[:print:]]*\b[[:alpha:]]\b[[:print:]]*\t|[[:upper:]][[:upper:]]/d' $file;
 # Remove duplicates from source
-cut -f2 $file | sed -e 's/[[:punct:]]//g' | sed 's/./\L&/g' | \
+cut -f2,3 $file | sed -e 's/[[:punct:]]//g' | sed 's/./\L&/g' | \
 paste $file - > $file.temp;
-sort -t$'\t' -k3 -u $file.temp | cut -f1,2,3,4 | shuf > $file && rm $file.temp;
+sort -t$'\t' -k4 -u $file.temp | cut -f1,2,3,4 | shuf > $file && rm $file.temp;
 ### Replace more or less than 30-200 characters.
 sed -r '/^.{,15}$|^.{200,}$/d' $file | shuf > all.tsv.clean && rm all.tsv.clean.temp;
